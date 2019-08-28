17572e79e6b438bb0d4b5d39c4ac1c6eb2e3002f
Jessica Yu <jessyu2010@gmail.com>
2019-08-27 21:30:58 -0700

    CSS styled components, basic mock-up of photo module display
    Images now display on a grid. Further work needed to adjust for listings with under 5 photos in listing data.

--- client/components/PhotosGrid.jsx
+++ client/components/PhotosGrid.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import PhotosGridEntry from './PhotosGridEntry.jsx';
+
+const GridDiv = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  width: 50%;
+  height: 100%;
+  padding: none; 
+  margin: none;
+`;
 
 class PhotosGrid extends React.Component {
   constructor(props) {
@@ -8,12 +20,31 @@ class PhotosGrid extends React.Component {
 
   render() {
     // photo grid
+    const { photos } = this.props;
+    console.log('Number of images:', photos.length);
+    if (photos.length > 5) {
+      return (
+        <GridDiv>
+          <PhotosGridEntry photo={photos[1]} />
+          <PhotosGridEntry photo={photos[2]} />
+          <PhotosGridEntry photo={photos[3]} />
+          <PhotosGridEntry photo={photos[4]} />
+        </GridDiv>
+      );
+    } 
+    const photosShift = photos.slice(1);
     return (
-      <div>
-      Photos Grid
-      </div>
-    );
+      <GridDiv>
+        {photosShift.map(photo => {
+          return <PhotosGridEntry photo={photo} />
+        })}
+      </GridDiv>
+    )
   }
 }
+
+PhotosGrid.propType = {
+  photos: PropTypes.array.isRequired,
+};
 
 export default PhotosGrid;
