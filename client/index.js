c73781fd8d219c93e006064ad7175dd5260040aa
Jessica Yu <jessyu2010@gmail.com>
2019-08-28 19:42:32 -0700

    Shallow tests implemented for the Photos component
    Tests the Photos component when no photos props passed to it, and also setting state with a 2 object array.

--- client/index.js
+++ client/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ test/Photos.test.js
@@ -0,0 +1,39 @@
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import Photos from '../client/components/Photos.jsx';
+import PhotosGrid from '../client/components/photosGrid.jsx';
+
+describe('<Photos>', () => {
+  it('<Photos> before API call', () => {
+    const component = shallow(
+      <Photos listingId={15} />, { disableLifecycleMethods: true }
+    );
+    expect(component.find('h3')).toHaveLength(1);
+  });
+
+  it('Photos takes photos state', () => {
+    const photos = [
+      {
+        "_id" : "5d64c1e10bd620e32d5071d8",
+        "slide_id" : 0,
+        "desc" : "Southern pochard. Citreoline trogon!",
+        "verified" : false,
+        "url" : "https://birdhouse325.s3-us-west-1.amazonaws.com/birdhouse032.png"
+      },
+      {
+        "_id" : "5d64c1e10bd620e32d5071d9",
+        "slide_id" : 1,
+        "desc" : "Red-vented barbet! White-bellied woodstar - Orange-fronted fruit dove.",
+        "verified" : true,
+        "url" : "https://birdhouse325.s3-us-west-1.amazonaws.com/birdhouse015.png"
+      }
+    ];
+    const component = shallow(
+      <Photos listingId={15} />, { disableLifecycleMethods: true }
+    );
+    component.setState({'photos': photos}, () => {
+      expect(component.state('photos')).toHaveLength(2);
+    });
+    
+  });
+});
